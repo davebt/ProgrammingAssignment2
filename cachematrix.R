95ad90b5169a3e85d2d3ad97bbf8f02eb5f2b590
David <davebt@inbox.com>
2015-08-22 22:29:55 +1000

    Wrote code for assignment
    Based on mean code but with inverses rather than means

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions create a special matrix that can keep 
+## cache of the matrices data and its inverse
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix creates a special matrix 
+## that is really a list containing a function to:
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse of the matrix
+## 4. get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     m <- NULL
+     set <- function(y) {
+          x <<- y
+          m <<- NULL
+     }
+     get <- function(x)
+     setinverse <- function(inverse) m <<- inverse
+     getinverse <- function() m
+     list( set = set, get = get,
+           setinverse = setinverse,
+           getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The second function cacheSolve computes the inverse of
+## the special matrix returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the 
+## matrix has not changed), then the cachesolve retrieves the 
+## inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+     m <- x$getinverse()
+     if(!is.null(m)) {
+          message("getting cached data")
+          return(m)
+     }
+     data <- x$get()
+     m <- solve(data)
+     x$setinverse(m)
+     m
 }
